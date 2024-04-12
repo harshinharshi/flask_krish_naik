549774ea1f49ca186744a662b6924ba97d2d2365
harshin ramesh <harshinharshi123@gmail.com>
2024-04-12 22:10:49 +0530

    rendering html

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask, redirect, url_for
+from flask import Flask, redirect, url_for, render_template, request
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return 'Hello World!'
+    return render_template('index.html')
 
 
 @app.route('/sucess/<int:age>')
@@ -25,6 +25,20 @@ def user_id(age):
     else:
         return redirect(url_for('fail', age=age))
 
+@app.route('/submit', methods=['POST', 'GET'])
+def submit():
+    if request.method == 'POST':
+        science = float(request.form['science'])
+        maths = float(request.form['maths'])
+        datascience = float(request.form['datascience'])
+        c = float(request.form['c'])
+        result = science + maths + datascience + c
+        if result > 50:
+            return render_template('result.html', result=result)
+        else:
+            return "failed"
+    else:
+        return redirect(url_for('index'))
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
